272bdd14b8e2101790242a9372e116af65b5f1f8
Mayorov Maxim <nub.pun@yandex.ru>
2016-11-07 10:28:03 +0500

    complete timer

--- timer/Program.cs
+++ timer/Program.cs
@@ -10,6 +10,80 @@ namespace timer
     {
         static void Main(string[] args)
         {
+            var timer = new Timer();
+            long s = 0;
+            using (timer.Start())
+            {
+                for (int i = 0; i < 111231445; i++)
+                    s += i - s + 1;
+
+            }
+            Console.WriteLine(timer.ElapsedMilliseconds);
+
+            using (timer.Continue())
+            {
+                for (int i = 0; i < 1231445; i++)
+                    s += i - s + 2;
+            }
+            Console.WriteLine(timer.ElapsedMilliseconds);
+        }
+    }
+
+    class Timer : IDisposable
+    {
+        DateTime start;
+        DateTime end;
+        long elapsedMilliseconds;
+        public void Dispose()
+        {
+            Stop();
+        }
+
+        public Timer()
+        {
+            elapsedMilliseconds = 0;
+        }
+
+        private bool isDisposed = false;
+        ~Timer()
+        {
+            Dispose(false);
+        }
+        protected virtual void Dispose(bool fromDisposeMethod)
+        {
+            if (!isDisposed)
+            {
+                if (fromDisposeMethod)
+                {
+                    Console.WriteLine("Очистка управляемых ресурсов в {0}");
+                }
+                Console.WriteLine("Очистка неуправляемых ресурсов в {0}");
+                isDisposed = true;
+            }
+        }
+        public Timer Start()
+        {
+            start = DateTime.Now;
+            return this;
+        }
+
+        public Timer Stop()
+        {
+            end = DateTime.Now;
+            elapsedMilliseconds += (end.Ticks - start.Ticks) / TimeSpan.TicksPerMillisecond;
+            return this;
+        }
+        public Timer Continue()
+        {
+            start = DateTime.Now;
+            return this;
+        }
+        public long ElapsedMilliseconds
+        {
+            get {
+                return elapsedMilliseconds;
+            }
+
         }
     }
 }
